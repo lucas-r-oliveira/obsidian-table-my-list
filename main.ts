d7023e700679af069a254b0b634720f09c4c1498
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2024-06-02 16:05:24 +0100

    parsing frontmatter

--- main.ts
+++ main.ts
@@ -1,32 +1,61 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting,  } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class TableMyPlugin extends Plugin {
+	activeFile = this.app.workspace.getActiveFile();
+	
+	async logFrontMatter() {
+		// The front matter is the properties/metadata part of 
+		// the note (in YAML format)
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		console.log("logging front matter...")
+	
+		
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			new Notice("There is no active file. Open a file first, and then try to use this command again.");
+			return;
+		}
+
+		await this.app.fileManager.processFrontMatter(activeFile, (frontMatter) => {
+			console.log(`frontMatter: ${JSON.stringify(frontMatter)}`);
+		})
+
+		return
+	}
 
 	async onload() {
-		await this.loadSettings();
+		this.addRibbonIcon('smile', "I'm here for testing purposes :)", () => new Notice("Hello, I am working") )
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (this.activeFile) {
+			new Notice("Hello I am active");
+			
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+
+			//FIXME:
+			//const frontMatter = this.extractFrontMatter(fileContent);
+			const frontMatter = this.extractFrontMatter()
+
+			if (frontMatter && frontMatter.TABLE_ME) {
+				// TODO: do something
+				console.log(frontMatter);
+			}
+		}
+
+		// when pressed, it creates a property called TABLE_ME -> it needs to be an editor callback then, right?
+		this.addCommand({
+			id: 'enable-table-my-file',
+			name: 'Table my file',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				//console.log(this.app.metadataCache;
+				this.logFrontMatter();
+			}
+		})
+		
+
+
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -65,9 +94,6 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
@@ -78,16 +104,11 @@ export default class MyPlugin extends Plugin {
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	onunload() {}
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+	//FIXME: mock
+	extractFrontMatter() {
+		return {TABLE_ME: true}
 	}
 }
 
@@ -104,31 +125,5 @@ class SampleModal extends Modal {
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
